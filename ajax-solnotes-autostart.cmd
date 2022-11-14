fba2060a530c72ebb14a1b18652ef69573c1ddc3
jjYBdx4IL <jjYBdx4IL@github.com>
2022-11-15 00:24:51 +0100

    fix

--- ajax-solnotes-autostart.cmd
+++ ajax-solnotes-autostart.cmd
@@ -2,4 +2,6 @@
 
 cd /d %~dp0
 
-e:\dev\cygwin\bin\screen.exe -dmS ajax-solnotes cmd /K nodemon -w server.js server.js -- --prod --managesolr --reporoot=%NOTESREPO% --portinc 1
+@rem e:\dev\cygwin\bin\screen.exe -dmS ajax-solnotes cmd /K nodemon -w server.js server.js -- --prod --managesolr --reporoot=%NOTESREPO% --portinc 1
+
+nodemon -w server.js server.js -- --prod --managesolr --reporoot=%NOTESREPO% --portinc 1
